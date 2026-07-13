c6578d5d36b986c628153726a8e649bc07084815
dekel <dekel.liviani@zoominfo.com>
2026-07-13 17:53:23 +0300

    add guard

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,12 +2,13 @@ import {Module} from "@nestjs/common";
 import {AuthController} from "./auth.controller";
 import {AuthService} from "./auth.service";
 import {JwtModule} from "@nestjs/jwt";
+import {JwtStrategy} from "./strategies/jwt.strategy";
 
 @Module({
     controllers: [AuthController],
     imports: [
         JwtModule.register({secret: process.env.JWT_SECRET, signOptions: { expiresIn: process.env.JWT_EXPIRES }}),
     ],
-    providers: [AuthService],
+    providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
